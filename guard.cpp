f861020a5b60abf1f0d8de0fc91195b2abd19117
AmirGill <amirg00@walla.com>
2022-05-20 19:01:09 +0300

    Implement guard design pattern

--- guard.cpp
+++ guard.cpp
@@ -1,1 +1,10 @@
 #include "guard.hpp"
+
+guard::guard(pthread_mutex_t& lock) {
+    _lock = lock;
+    pthread_mutex_lock(&_lock);
+}
+
+guard::~guard() {
+    pthread_mutex_unlock(&_lock);
+}
--- guard.hpp
+++ guard.hpp
@@ -1,5 +1,9 @@
-
+#include "pthread.h"
 
 class guard {
-
+private:
+    pthread_mutex_t _lock{};
+public:
+    explicit guard(pthread_mutex_t &lock);
+    ~guard();
 };
