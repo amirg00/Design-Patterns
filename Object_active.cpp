fd3047d643ed2d37d12270203ed21c433605d9a2
AmirGill <amirg00@walla.com>
2022-06-05 00:13:54 +0300

    Implement newAO and destroyAO for the active_object.

--- Object_active.cpp
+++ Object_active.cpp
@@ -1,10 +1,25 @@
 #include "Object_active.hpp"
 
-void newAO(Deque* queue, void (*treat)(void*), void (*end_treat)()){
+AO_ptr newAO(Deque* queue, void (*treat)(void*), void (*end_treat)()){
+    AO_ptr active_object = (AO_ptr) malloc(sizeof(AO));
 
+    /*Fill Active Object's fields*/
+    active_object->treat = treat;
+    active_object->end_treat = end_treat;
+    active_object->queue = queue;
+
+    // Create rector's thread id
+    if (pthread_create(&active_object->private_thread, NULL, eventExecute, active_object) != 0) {
+        printf("Unable to create thread\n");
+    }
+    return active_object;
 }
+
 void destroyAO(AO_ptr AO){
+    pthread_join(AO->private_thread, NULL); // Wait for thread to finish everything before canceling.
+    pthread_cancel(AO->private_thread);
     destoryQ(AO->queue);
+    free(AO);
 }
 
 void eventExecute(AO_ptr AO){
