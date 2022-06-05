ea1b2397dfbea3a9f198be28d5c3615dc7c0412e
AmirGill <amirg00@walla.com>
2022-06-05 12:44:29 +0300

    Implement simple poll client gets string and send to poll server.

--- pollclient.cpp
+++ pollclient.cpp
@@ -1,4 +1,89 @@
-//
-// Created by Amir on 05/06/2022.
-//
+/*
+* client.c -- a stream socket client demo.
+* This will be a part of the reactor's test.
+*/
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+#include <arpa/inet.h>
+
+#define PORT "3490" // the port client will be connecting to
+
+#define MAX_DATA_SIZE 1024 // max number of bytes we can get at once
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa) {
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in *) sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6 *) sa)->sin6_addr);
+}
+
+int main(int argc, char *argv[]) {
+    int sockfd, numbytes;
+    char buf[MAX_DATA_SIZE];
+    struct addrinfo hints, *servinfo, *p;
+    int rv;
+    char s[INET6_ADDRSTRLEN];
+
+    if (argc != 2) {
+        fprintf(stderr, "usage: client hostname\n");
+        exit(1);
+    }
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
+    if ((rv = getaddrinfo(argv[1], PORT, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
+
+    // loop through all the results and connect to the first we can
+    for (p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                             p->ai_protocol)) == -1) {
+            perror("client: socket");
+            continue;
+        }
+
+        if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
+            perror("client: connect");
+            continue;
+        }
+
+        break;
+    }
+
+    if (p == NULL) {
+        fprintf(stderr, "client: failed to connect\n");
+        return 2;
+    }
+
+    inet_ntop(p->ai_family, get_in_addr((struct sockaddr *) p->ai_addr) s, sizeof s);
+    printf("client: connecting to %s\n", s);
+    freeaddrinfo(servinfo); // all done with this structure
+
+    // Get input string from client, then it
+    // sends the data to the poll server.
+    for (;;) {
+        memset(buf, 0, sizeof(buf)); // clean buffer
+        gets(buf); // get input string from client
+        send(sockfd, buf, strlen(buf)); // send data to server.
+        printf("client: sends '%s'\n", buf);
+    }
+
+    close(sockfd);
+    return 0;
+}
