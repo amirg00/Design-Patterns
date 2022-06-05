57f95fe8f38edeb1056ea00ec834bddebe1c4056
AmirGill <amirg00@walla.com>
2022-06-05 11:31:20 +0300

    Test application for guard.

---
+++ guard_app_test.cpp
@@ -0,0 +1,36 @@
+#include "guard.hpp"
+using namespace std;
+
+/**
+ * Brief: this application test increases a global int variable.
+ *        In order to see the lock effect, we sleep the thread,
+ *        which locked the function, to see that didn't increase
+ *        the variable at the same time.
+ */
+
+pthread_mutex_t lock; // lock for the guard.
+int *global_num = (int*) malloc(sizeof(int)); // global int variable, which will be increased by threads.
+
+// Increases global int by 1.
+void update_global_var(){
+    guard guard{lock};
+    (*global_num)++; //increase global by one.
+    cout << "Variable's Current Value: " << *global_num << endl;
+    sleep(5);
+}
+
+int main(){
+    *global_num = 0; // Initial variable's value is set to zero.
+
+    // Initialize the lock.
+    if (pthread_mutex_init(&lock, NULL) != 0) {
+        printf("\n mutex init failed\n");
+        return 1;
+    }
+    pthread_t pthread_1, pthread_2;
+    pthread_create(&pthread_1, NULL,  update_global_var);
+    pthread_create(&pthread_2, NULL,  update_global_var);
+    pthread_join(pthread_1, NULL);
+    pthread_join(pthread_2, NULL);
+    free(global_num);
+}
